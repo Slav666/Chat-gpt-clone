cb9d3551f6717d335e8309d7f150fc7598882e8a
Slawomir Dyk <slawomir.dyk@astrosat.net>
2023-08-31 17:59:25 +0100

    Build out the chat page
    Created the basic chat page layout, ctreated the ChatSidebar component and the footer and the message form.

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,12 +1,41 @@
 import Head from "next/head";
+import { ChatSidebar } from "components/chatSideBar";
+import React, { useState } from "react";
 
-export default function Home() {
+export default function ChatPage() {
+  const [messageText, setMessageText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("MESSAGE TEXT:", messageText);
+  };
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>New chat</title>
       </Head>
-      <h1>Welcome to the chat id page</h1>
-    </div>
+      <div className="grid h-screen grid-cols-[260px_1fr]">
+        <ChatSidebar />
+        <div className="flex flex-col bg-gray-700">
+          <div className="flex-1">Chat window</div>
+          <footer className="bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+              <fieldset className="flex gap-2">
+                <textarea
+                  value={messageText}
+                  onChange={(e) => setMessageText(e.target.value)}
+                  placeholder="Send a message..."
+                  className="w-full resize-none rounded-md bg-gray-700 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
+                />
+                <button type="submit" className="btn">
+                  Send
+                </button>
+              </fieldset>
+            </form>
+          </footer>
+        </div>
+      </div>
+    </>
   );
 }
