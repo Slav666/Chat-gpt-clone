c241ba570667c53064dbefaf2518edcee7211543
Slawomir Dyk <slawomir.dyk@astrosat.net>
2023-09-02 12:08:18 +0100

    build the openAI streaming functionality
    I created openAI API key. I created the sendMessage endpoint and set up openAI API call. I stream openAI API response on the front-end and I created the message component and display new messeges in the chat window. I added routes protection mechanism so only logged in users can  access. I added css styling.

---
+++ pages/api/chat/sendMessage.js
@@ -0,0 +1,36 @@
+import { OpenAIEdgeStream } from "openai-edge-stream";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req) {
+  try {
+    const { message } = await req.json();
+
+    const initialChatMessage = {
+      role: "system",
+      content:
+        "Your name is Chatty Slav. An incredibly intelligent and quick-thinking AI, that always replies with an enthusiastic and positive energy. You were created by WebDevEducation. Your response must be formatted as markdown.",
+    };
+
+    const stream = await OpenAIEdgeStream(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [initialChatMessage, { content: message, role: "user" }],
+          stream: true,
+        }),
+      }
+    );
+    return new Response(stream);
+  } catch (err) {
+    console.log("Error in send message", err);
+  }
+}
--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,13 +1,50 @@
 import Head from "next/head";
 import { ChatSidebar } from "components/chatSideBar";
 import React, { useState } from "react";
+import { streamReader } from "openai-edge-stream";
+import { v4 as uuid } from "uuid";
+import { Message } from "components/Message/Message";
 
 export default function ChatPage() {
+  const [incomingChatMessage, setIncomingChatMessage] = useState("");
   const [messageText, setMessageText] = useState("");
+  const [newChatMessages, setNewChatMessages] = useState([]);
+  const [generatingResponse, setGeneratingResponse] = useState(false);
 
-  const handleSubmit = (e) => {
+  console.log("here:", messageText);
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("MESSAGE TEXT:", messageText);
+    setGeneratingResponse(true);
+    setNewChatMessages((prev) => {
+      const newChatMessages = [
+        ...prev,
+        {
+          _id: uuid(),
+          role: "user",
+          content: messageText,
+        },
+      ];
+      return newChatMessages;
+    });
+    setMessageText("");
+
+    const response = await fetch(`/api/chat/sendMessage`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ message: messageText }),
+    });
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+    const reader = data.getReader();
+    await streamReader(reader, (message) => {
+      console.log(message);
+      setIncomingChatMessage((s) => `${s}${message.content}`);
+    });
+    setGeneratingResponse(false);
   };
 
   return (
@@ -17,16 +54,27 @@ export default function ChatPage() {
       </Head>
       <div className="grid h-screen grid-cols-[260px_1fr]">
         <ChatSidebar />
-        <div className="flex flex-col bg-gray-700">
-          <div className="flex-1">Chat window</div>
+        <div className="flex flex-col overflow-hidden bg-gray-700">
+          <div className="flex-1 overflow-scroll text-white">
+            {newChatMessages.map((message) => (
+              <Message
+                key={message._id}
+                role={message.role}
+                content={message.content}
+              />
+            ))}
+            {!!incomingChatMessage && (
+              <Message role="assistant" content={incomingChatMessage} />
+            )}
+          </div>
           <footer className="bg-gray-800 p-10">
             <form onSubmit={handleSubmit}>
-              <fieldset className="flex gap-2">
+              <fieldset className="flex gap-2" disabled={generatingResponse}>
                 <textarea
                   value={messageText}
                   onChange={(e) => setMessageText(e.target.value)}
-                  placeholder="Send a message..."
-                  className="w-full resize-none rounded-md bg-gray-700 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
+                  placeholder={generatingResponse ? "" : "Send a message..."}
+                  className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
                 />
                 <button type="submit" className="btn">
                   Send
